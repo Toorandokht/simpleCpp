eaa85400a1d8c0e73df7580fa09e5503eb0eed1d
Toorandokht Tofangchi <t.tofangchi@outlook.com>
2023-04-30 09:36:09 +0330

    Add files via upload

---
+++ 20- Show the Greatest common divisor(GCD) of a number/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+//Show the Greatest common divisor(GCD) of a number
+using namespace std;
+
+int a,b,t,r;
+int main(int argc, char** argv)
+{
+	cout<<"enter 1st integer number: " << "a= ";
+	cin>>a;
+	cout<<"enter 2nd integer number: " << "b= ";
+	cin>>b;
+	
+	cout<<"the common divisors for "<< a <<" and "<< b << " include:\n";
+	
+	for(t=1;t<=a+1;t++)
+	{
+	
+		if((a%t==0) && (b%t==0))
+		{
+			cout<<t<<endl;	
+		
+		}
+	
+	}
+	
+	cout<<"the greatest common divisor(GCD) for "<< a <<" and "<< b << " is:\n";
+	r=a%b;
+	while(r!=0)
+	{
+	a=b;
+	b=r;
+	r=a%b;
+	}
+	cout<<b;
+	
+	return 0;
+}
