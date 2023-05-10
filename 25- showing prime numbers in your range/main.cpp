1b4fdfe14339386cf401c80b4026442a52787502
Toorandokht Tofangchi <t.tofangchi@outlook.com>
2023-05-10 10:57:46 +0330

    Add files via upload

---
+++ 25- showing prime numbers in your range/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// showing prime numbers in your range
+using namespace std;
+
+int t,i,a,b,n;
+int main(int argc, char** argv)
+{
+	cout<<"enter the lower number of your range"<<endl;
+	cin>>a;
+	cout<<"enter the higher number of your range"<<endl;
+	cin>>b;
+	
+for(i=a;i<=b;i++)
+{
+	for(t=1;t<=b;t++)
+	{
+		if(i%t==0)
+		{	
+			n++;		
+		}
+	}
+		if (n==2)
+		{
+			cout<< i << " is a prime number"<<endl;
+		}
+		
+		n=0;	
+}
+
+	return 0;
+}
