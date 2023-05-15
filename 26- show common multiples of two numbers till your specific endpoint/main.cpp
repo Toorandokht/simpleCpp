aba4037707a2b0bf83e99aa90ee05f1f25064e30
Toorandokht Tofangchi <t.tofangchi@outlook.com>
2023-05-15 23:05:22 +0330

    Add files via upload

---
+++ 26- show common multiples of two numbers till your specific endpoint/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// show common multiples of two numbers  till your specific endpoint
+using namespace std;
+
+int t,a,b,e;
+int main(int argc, char** argv)
+{
+	cout<<"enter you first(lower number) to see the multiples"<<endl;
+	cin>>a;
+	cout<<"enter you second(higher number) to see the multiples"<<endl;
+	cin>>b;
+	cout<<"enter the endpoint of your range"<<endl;
+	cin>>e;
+	
+	for(t=a;t<=e;t++)
+	{
+		if((t%a==0) && (t%b==0))
+			
+		cout<< t <<endl;
+	}
+			
+	return 0;
+}
