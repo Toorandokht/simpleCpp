920de76ae988e7ff04a3c9fbac005cef5ddab082
Toorandokht Tofangchi <t.tofangchi@outlook.com>
2023-05-20 14:05:27 +0330

    Update main.cpp

--- 11- Vowels & Consonant & numbers & other Characters/main.cpp
+++ 11- Vowels & Consonant & numbers & other Characters/main.cpp
@@ -7,33 +7,32 @@ using namespace std;
 char str[1000];
 int i;
 
-int main(int argc, char** argv)
+int main()
 {
 	cout<<"enter your character: ";
 	cin>>str;
 	
 	
-		if(	str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'
-		||	str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'	)
+	if(	str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'
+	||	str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'	)
 	{
-		cout<< str << " is one of the vowels";
+	cout<< str << " is one of the vowels";
 	}
 	
-		else if(	(str[i]>='a') && (str[i]<='z') || (str[i]>='A') && (str[i]<='Z' )	)
-		{
-		cout<< str << " is one of the consonants";
-		}
+	else if(	(str[i]>='a') && (str[i]<='z') || (str[i]>='A') && (str[i]<='Z' )	)
+	{
+	cout<< str << " is one of the consonants";
+	}
 	
-		else if(	(str[i]>='0') && (str[i]<='9') )
-		{
-		cout<< str << " is a digit";
-		}
+	else if(	(str[i]>='0') && (str[i]<='9') )
+	{
+	cout<< str << " is a digit";
+	}
 	
-		else
-		{
-		cout<< str << " is one of the other characters";
-		}
-
+	else
+	{
+	cout<< str << " is one of the other characters";
+	}
 	
 	return 0;
 }
