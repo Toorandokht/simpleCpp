4609eae3b7651cd72c909dbaf7cbe54c213dca16
Toorandokht Tofangchi <t.tofangchi@outlook.com>
2023-05-22 11:32:44 +0330

    Update main.cpp

--- 18- Show whether a number is prime or not/main.cpp
+++ 18- Show whether a number is prime or not/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int t,a,n;
-int main(int argc, char** argv)
+int main()
 {
 	cout<<"enter an integer number: " << "a= ";
 	cin>>a;
@@ -15,10 +15,8 @@ int main(int argc, char** argv)
 		
 		if(a%t==0)
 		{
-			
 			cout<<t<<endl;
-			n++;
-				
+			n++;	
 		}
 		
 	}
